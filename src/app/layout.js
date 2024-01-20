1cfc0bcd251e06bd2fa9d52366d1580d6f5197f9
nathan-miranda-soma <20144892+onathanmiranda@users.noreply.github.com>
2024-01-20 14:13:42 -0300

    Fix Background

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,20 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import Image from 'next/image';
-import BackgroundImage from '../images/Burlesqueria_Teatro_Café_Pequeno_por_Renan_Olivetti.jpeg';
-
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body className={`bg-black`}>
-        <Image 
-          className='fixed top-0 left-0 w-full h-full object-cover object-center z-0 opacity-70'
-          src={BackgroundImage} 
-          alt="Burlesqueria no Teatro Café Pequeno" 
-        />
+      <body 
+        style={{
+          backgroundImage: `url("Burlesqueria_Teatro_Café_Pequeno_por_Renan_Olivetti.jpeg")`,
+          backgroundPosition: `center center`,
+          backgroundAttachment: 'fixed',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat'
+        }}
+        className={`bg-black`}>
         {children}
       </body>
     </html>
