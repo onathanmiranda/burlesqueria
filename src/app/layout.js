72586aa2b20350f1f400827d1d50d36cf16a8cc7
nathan-miranda-soma <20144892+onathanmiranda@users.noreply.github.com>
2024-01-21 14:31:25 -0300

    Adds GA

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
-const inter = Inter({ subsets: ["latin"] });
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
@@ -17,6 +16,7 @@ export default function RootLayout({ children }) {
         className={`bg-black`}>
         {children}
       </body>
+      <GoogleAnalytics gaId="G-L41WDCH6HK" />
     </html>
   );
 }
